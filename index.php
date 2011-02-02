db63e681aaeadba69a82f7b5b9ae4e0b91e80522
William Duyck <fuzzyfox0@gmail.com>
2011-02-02 13:07:32 +0000

    updated index.php in prep for future work

--- index.php
+++ index.php
@@ -1,9 +1,25 @@
 <?php
 	
-	/**
-	 * Simple include of index.html till the file is created propperly. A
-	 * placeholder if you will
-	 */
-	include('./index.html');
+	//get the glue library
+	require_once(dirname(__FILE__).'/app/libraries/glue.lib.php');
 	
+	//setup url mapping with the help of glue
+	$urls = array(
+		'/' => 'index'
+	);
+	
+	/**
+	 * index
+	 *
+	 * the controller for all things relating to the main index page of the app
+	 */
+	class Index
+	{
+		function GET()
+		{
+			include('./index.html');
+		}
+	}
+	
+	glue::stick($urls);
 ?>
