619f31493e5646169ecfe7b3800f020fbfbca903
William Duyck <fuzzyfox0@gmail.com>
2011-03-08 17:17:52 +0000

    no significant change but machine change needed

--- app/views/backend/dashboard.php
+++ app/views/backend/dashboard.php
@@ -3,6 +3,11 @@
 		<head>
 			<?php $this->head('Dashboard'); ?>
 			<?php $this->script('common'); ?>
+			<?php $this->script('graph/raphael'); ?>
+			<?php $this->script('graph/g.raphael'); ?>
+			<?php $this->script('graph/g.bar'); ?>
+			<?php $this->script('graph/g.line'); ?>
+			<?php $this->script('graph/g.pie'); ?>
 		</head>
 		<body>
 			<div id="header">
@@ -16,9 +21,7 @@
 			<div class="section">
 				<div class="article">
 					<h2>Dashboard</h2>
-					<div class="section left">
-						
-					</div>
+					<div class="section left" id="graph"></div>
 					<div class="section aside left"></div>
 					<div class="section aside left"></div>
 					<div class="clear">&nbsp;</div>
@@ -30,7 +33,14 @@
 			</div>
 			
 			<script type="text/javascript">
-				
+				$c.onload(function(){
+					var $r = Raphael('graph'),
+					data = [[12, 30, 21], [20, 14, 26], [12, 35, 24]];
+					
+					$r.g.text(100, 10, 'example graph');
+					
+					$r.g.barchart(10, 10, 380, 320, data);
+				});
 			</script>
 		</body>
 	</html>
