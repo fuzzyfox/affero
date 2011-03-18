620d384afb287a4bba6c9ea1b8c3090000faa92c
William D <fuzzyfox0@googlemail.com>
2011-03-18 11:36:37 +0000

    more work on graphing, bars and pies now available for use, lines left to complete then getting the data to feed them

--- app/views/backend/dashboard.php
+++ app/views/backend/dashboard.php
@@ -8,6 +8,7 @@
 			<?php $this->script('graph/g.bar'); ?>
 			<?php $this->script('graph/g.line'); ?>
 			<?php $this->script('graph/g.pie'); ?>
+			<?php $this->script('graph/dashboard'); ?>
 		</head>
 		<body>
 			<div id="header">
@@ -22,9 +23,11 @@
 				<div class="article">
 					<h2>Dashboard</h2>
 					<div class="section left" id="graph" style="width:400px;height:340px">
-						<h2>Graph</h2>
+						<p class='caution'>Loading Graph...</p>
 					</div>
-					<div class="section aside left" id="legend"></div>
+					<div class="section aside left" id="legend">
+						<p class='caution'>Loading Legend...</p>
+					</div>
 					<div class="section aside left"></div>
 					<div class="clear">&nbsp;</div>
 				</div>
@@ -35,34 +38,7 @@
 			</div>
 			
 			<script type="text/javascript">
-			$c.onload(function(){
-					var data = [[10, 20, 25], [15, 25, 30], [20, 30, 35], [35, 35, 35]],
-					chart = function(container, data)
-					{
-						//clear previous chart
-						document.getElementById(container).innerHTML = '';
-						document.getElementById('legend').innerHTML = '';
-						//create new chart
-						window.$r = Raphael(container, 380, 320);
-						//plot chart
-						$r.g.barchart(0, 0, 380, 320, data);
-						//plot axis
-						$r.g.axis(20, 300, 280, 0, 35, 7, 1);
-						$r.g.axis(20, 300, 339, null, null, 6, 0, [' ', 'past', ' ', 'present', ' ', 'future', ' '], '.', 0);
-						
-						//get colors for legend
-						var bars = document.getElementById(container).getElementsByTagName('svg')[0].getElementsByTagName('path');
-						for(var i = 0; i < data.length; i++)
-						{
-							document.getElementById('legend').innerHTML += '<div id="bar-'+i+'"></div>';
-							var paper = Raphael('bar-'+i, 30, 30);
-							paper.path('M21.25,8.375V28h6.5V8.375H21.25zM12.25,28h6.5V4.125h-6.5V28zM3.25,28h6.5V12.625h-6.5V28z').attr({fill:bars[i].getAttribute('fill'), stroke:'none'});
-							document.getElementById('bar-'+i).innerHTML += '<p>Data Set '+i+'</p>';
-							document.getElementById('bar-'+i).getElementsByTagName('svg')[0].setAttribute('class', 'right');
-						}
-					};
-					chart('graph', data);
-			});
+				
 			</script>
 		</body>
 	</html>
